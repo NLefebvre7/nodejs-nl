ec2919aef65aa0753a9905b80127303d3c7c6d08
NLefebvre7 <57395815+NLefebvre7@users.noreply.github.com>
2020-11-24 10:42:31 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,6 +13,10 @@ server.use(bodyParser.urlencoded());
 server.use(bodyParser.json());
 
 const postRoute = require('./api/routes/postRoute');
+
 postRoute(server);
+
+const commentRoute = require('./api/routes/commentRoute');
+commentRoute(server);
 
 server.listen(port, hostname);
